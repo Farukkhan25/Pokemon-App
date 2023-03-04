57cb79b5ca49c721a09cd2585835f44b227a4755
Farukkhan25 <programmingherofaruk@gmail.com>
2023-03-04 21:32:58 +0600

    Route setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import PokemonList from "./components/PokemonList";
+import PokemonDetails from "./components/PokemonDetails";
 
 function App() {
   return (
     <div className="App">
-     
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/pokemon" element={<PokemonList />} />
+          <Route path="/pokemon/:id" element={<PokemonDetails />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
